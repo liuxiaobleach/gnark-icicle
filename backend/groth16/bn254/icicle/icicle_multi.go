d28a2b15f7ef03a8dbdc5b27d2121cfdfcb661e4
liuxiaobleach <1241368737@qq.com>
2024-04-30 10:55:08 +0800

    use one

--- backend/groth16/bn254/icicle/icicle_multi.go
+++ backend/groth16/bn254/icicle/icicle_multi.go
@@ -82,7 +82,7 @@ func (pk *ProvingKey) setupDevicePointersOnMulti() error {
 	/*************************  Start G1 Device Setup  ***************************/
 	/*************************     A      ***************************/
 	copyADone := make(chan bool, 1)
-	icicle_cr.RunOnDevice(0, func(args ...any) {
+	icicle_cr.RunOnDevice(1, func(args ...any) {
 		g1AHost := (icicle_core.HostSlice[curve.G1Affine])(pk.G1.A)
 		g1AHost.CopyToDevice(&pk.G1Device.A, true)
 		copyADone <- true
@@ -377,7 +377,7 @@ func ProveOnMulti(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, op
 
 	// schedule our proof part computations
 	arDone := make(chan error, 1)
-	icicle_cr.RunOnDevice(0, func(args ...any) {
+	icicle_cr.RunOnDevice(1, func(args ...any) {
 		arDone <- computeAR1()
 	})
 	<-arDone

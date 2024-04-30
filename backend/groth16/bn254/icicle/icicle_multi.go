4976bc98f8d5e1e5240b3aeb424670c75250d4bc
liuxiaobleach <1241368737@qq.com>
2024-04-30 11:33:56 +0800

    update

--- backend/groth16/bn254/icicle/icicle_multi.go
+++ backend/groth16/bn254/icicle/icicle_multi.go
@@ -413,9 +413,7 @@ func ProveOnMulti(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, op
 	})
 	icicle_cr.RunOnDevice(0, func(args ...any) {
 		wireValuesBDevice.Free()
-		//h.Free()
+		h.Free()
 	})
-
-	time.Sleep(1 * time.Second)
 	return proof, nil
 }

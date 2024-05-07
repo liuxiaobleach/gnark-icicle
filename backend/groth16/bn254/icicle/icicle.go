668ab17a1f466e29aee9da746db0f2dd7335fd39
liuxiaobleach <1241368737@qq.com>
2024-05-07 11:12:10 +0800

    update

--- backend/groth16/bn254/icicle/icicle.go
+++ backend/groth16/bn254/icicle/icicle.go
@@ -460,10 +460,10 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		return nil, err
 	}
 	wireValuesBDevice.Free()
-	h.Free()
 	if err := computeKRS(); err != nil {
 		return nil, err
 	}
+	h.Free()
 
 	log.Debug().Dur("took", time.Since(start)).Msg("prover done")
 

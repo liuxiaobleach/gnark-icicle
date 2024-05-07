797519adcfa36c5f8a7d715460c39753abf49f26
liuxiaobleach <1241368737@qq.com>
2024-05-07 10:50:38 +0800

    update

--- backend/groth16/bn254/icicle/icicle.go
+++ backend/groth16/bn254/icicle/icicle.go
@@ -474,9 +474,9 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		wireValuesBDevice.Free()
 		h.Free()
 	}()*/
-	wireValuesADevice.Free()
-	wireValuesBDevice.Free()
-	h.Free()
+	//wireValuesADevice.Free()
+	//wireValuesBDevice.Free()
+	//h.Free()
 
 	return proof, nil
 }

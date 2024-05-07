1bc7dcce97e4fe8d788f26683bb6b02682339f74
liuxiaobleach <1241368737@qq.com>
2024-05-07 10:48:36 +0800

    update

--- backend/groth16/bn254/icicle/icicle.go
+++ backend/groth16/bn254/icicle/icicle.go
@@ -396,6 +396,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		toRemove := commitmentInfo.GetPrivateCommitted()
 		toRemove = append(toRemove, commitmentInfo.CommitmentIndexes())
 		_wireValues := filterHeap(wireValues[r1cs.GetNbPublicVariables():], r1cs.GetNbPublicVariables(), internal.ConcatAll(toRemove...))
+		log.Debug().Msg(fmt.Sprintf("_wireValues fr len: %d", len(_wireValues)))
 		_wireValuesHost := (icicle_core.HostSlice[fr.Element])(_wireValues)
 		resKrs := make(icicle_core.HostSlice[icicle_bn254.Projective], 1)
 		cfg.AreScalarsMontgomeryForm = true
@@ -452,13 +453,16 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 	if err := computeAR1(); err != nil {
 		return nil, err
 	}
+	wireValuesADevice.Free()
 	if err := computeBS1(); err != nil {
 		return nil, err
 	}
-	if err := computeKRS(); err != nil {
+	if err := computeBS2(); err != nil {
 		return nil, err
 	}
-	if err := computeBS2(); err != nil {
+	wireValuesBDevice.Free()
+	h.Free()
+	if err := computeKRS(); err != nil {
 		return nil, err
 	}
 
